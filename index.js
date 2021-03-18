6ff0ca9f3fc9bdb75faa9223cbd8b6d123a8822c
GrahamSH <graham.s.howard@gmail.com>
2021-03-18 18:58:42 +0000

    Init

--- index.js
+++ index.js
@@ -1,45 +1,73 @@
-const app = require('express')()
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
-let jsoning = require('jsoning');
-let db = new jsoning(__dirname + "/database.json");
+require("dotenv").config();
+const app = require("express")();
+const server = require("http").createServer(app);
+const io = require("socket.io")(server);
 const imgbbUploader = require("imgbb-uploader");
+const emoji = require("discord-emoji-converter");
+const monk = require("monk");
+const db = monk(process.env.DB);
+const { toHTML } = require("discord-markdown");
+var users = [];
+(async () => {
+  const messages = db.get("messages");
 
+  io.on("connection", async (socket) => {
+    io.emit("users", users);
+    socket.on("disconnect", (reason) => {
+      users = users.filter(function (value, index, arr) {
+        return value.id !== socket.id;
+      });
+      io.emit("users", users);
+    });
 
+    socket.on("login", function (user) {
+      users.push({ name: user.name, id: socket.id });
+      io.emit("users", users);
+    });
 
-(async() => {
+    allMessages = await messages.find();
 
-io.on('connection', async (socket) => {
-    messages = await db.get("messages")
-  for (message of messages){
-    socket.emit('new_message', message)
-  }
+    for (message of allMessages) {
+      message.old = true;
 
-  socket.on('chat message', async (msg) => {
-    if (msg.password = process.env.PASS) {
-      if (msg.images.length) {
-let upload = await imgbbUploader({
-  apiKey: process.env.IMGBB_API_KEY, // MANDATORY
-  name: "image",
-  base64string: msg.images[0].split(',')[1],
-})
-  .catch((error) => console.error(error));
-console.log(upload)
-msg.images = [upload.image.url]
-}
+      socket.on("chat message", async (msg) => {
+        try {
 
-      msg.password = 'REDACTED'
-      console.log('message: ' + msg);
-      console.log(msg.images)
-      
-      io.emit('new_message', msg)
-      await db.push("messages", msg);
+        if (msg.password === process.env.PASS) {
+
+          console.log("HI");
+          if (msg.images.length) {
+            let upload = await imgbbUploader({
+              apiKey: process.env.IMGBB_API_KEY, // MANDATORY
+              name: "image",
+              base64string: msg.images[0].split(",")[1],
+            }).catch((error) => console.error(error));
+            console.log(upload);
+            msg.images = [upload.image.url];
+          }
+          msg.msg = emoji.emojify(toHTML(msg.msg));
+          msg.password = "REDACTED";
+          console.log("message: " + msg);
+          console.log(msg.images);
+
+          io.emit("new_message", msg);
+          messages.insert(msg);
+        }
+        } catch (err) {
+          console.error(err)
+        }
+      });
     }
-
   });
-});
-app.get('/', (req, res) => {
-  res.sendFile(`${__dirname}/index.html`)
-})
-server.listen(3000);
+  app.get("/", (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+  });
+  app.get("/ping.mp3", (req, res) => {
+    res.sendFile(`${__dirname}/ping.mp3`);
+  });
+  app.get("/icon.svg", (req, res) => {
+    res.sendFile(`${__dirname}/icon.svg`);
+  });
 })();
+server.listen(3000);
+console.log("Started");
